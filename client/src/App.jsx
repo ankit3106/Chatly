24a8b8927c01ba8d6510f8b77ec90cc806744942
Ankit <luffytarow13@gmail.com>
2025-06-21 18:24:53 +0530

    home page and sidebar initial commits

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage.jsx' 
+import LoginPage from './pages/LoginPage.jsx'
+import ProfilePage from './pages/ProfilePage.jsx'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className="bg-[#161717] text-white min-h-screen">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+    </div>
   )
 }
 
