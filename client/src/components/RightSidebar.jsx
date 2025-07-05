36cd2fecfa3b63a901567fa24e44a9c3634489be
Ankit <120703430+ankit3106@users.noreply.github.com>
2025-07-05 15:03:58 +0000

    all done finished

--- client/src/components/RightSidebar.jsx
+++ client/src/components/RightSidebar.jsx
@@ -1,11 +1,22 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import assets from '../assets/assets.js'
 import { imagesDummyData } from '../assets/assets.js'
 import { AuthContext } from '../../context/AuthContext.jsx'
+import ChatContainer from './ChatContainer.jsx'
+import { ChatContext } from '../../context/ChatContext.jsx'
 
-const RightSidebar = ({ selectedUser }) => {
+const RightSidebar = () => {
 
-    const {logout} = useContext(AuthContext);
+    const { selectedUser, messages } = useContext(ChatContext);
+    const { logout, onlineUsers } = useContext(AuthContext);
+
+    const [msgImages, setMsgImages] = useState([]);
+
+    //Get all images form the messages and set them to state
+
+    useEffect(() => {
+        setMsgImages(messages.filter(msg => msg.image).map(msg => msg.image))
+    }, [messages])
 
     return selectedUser && (
         <div className={`bg-[#8185B2]/10 text-white w-full h-full relative overflow-y-auto ${selectedUser ? "max-md:hidden" : ""}`}>
@@ -16,7 +27,7 @@ const RightSidebar = ({ selectedUser }) => {
                     className="w-24 h-24 rounded-full object-cover border-none"
                 />
                 <div className="flex items-center gap-2">
-                    <span className="w-3 h-3 rounded-full bg-green-500 inline-block"></span>
+                    {onlineUsers.includes(selectedUser._id) && <span className="w-3 h-3 rounded-full bg-green-500 inline-block"></span>}
                     <span className="text-xl font-semibold">{selectedUser?.fullName}</span>
                 </div>
                 <p className="text-sm text-gray-300 text-center mt-2">{selectedUser?.bio || "No bio available."}</p>
@@ -27,14 +38,14 @@ const RightSidebar = ({ selectedUser }) => {
             <div className="px-5 text-xs">
                 <p>Media</p>
                 <div className='mt-2 max-h-[200px] overflow-y-scroll grid grid-cols-2 gap-4 opacity-80'>
-                    {imagesDummyData.map((url, index) => (
+                    {msgImages.map((url, index) => (
                         <div key={index} onClick={() => window.open(url)} className='cursor-pointer rounded'>
                             <img src={url} alt="" className='h-full rounded-md' />
                         </div>
                     ))}
                 </div>
             </div>
-            <button onClick={()=> logout()}  className='absolute bottom-5 left-1/2 transform -translate-x-1/2 bg-gradient-to-r from-purple-400 to-violet-600 text-white border-none text-sm font-light py-2 px-20 rounded-full cursor-pointer'>
+            <button onClick={() => logout()} className='absolute bottom-5 left-1/2 transform -translate-x-1/2 bg-gradient-to-r from-purple-400 to-violet-600 text-white border-none text-sm font-light py-2 px-20 rounded-full cursor-pointer'>
                 Logout
             </button>
         </div>
